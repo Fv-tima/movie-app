582f882ead40b45c3964c442026c28c7e78b9ca9
Oluwalogbon Fatima <zahraoluwalogbon@gmail.com>
2022-10-30 20:24:32 +0100

    First commit of movie app

---
+++ main.js
@@ -0,0 +1,62 @@
+const API_URL =
+    "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=fdb85c2b4a94a30bb203bf0442bd8d42";
+const IMG_PATH = "https://image.tmdb.org/t/p/w1280";
+const SEARCH_API =
+  "https://api.themoviedb.org/3/search/movie?&api_key=fdb85c2b4a94a30bb203bf0442bd8d42&query=";
+
+
+
+const main = document.getElementById("main");
+const search = document.getElementById("search");
+const form = document.getElementById("form");
+
+getMovies(API_URL)
+
+async function getMovies(url) {
+    const res = await fetch(url)
+    const data = await res.json()
+    showMovies(data.results)
+}
+
+function showMovies(movies) {
+    main.innerHTML = ''
+    movies.forEach((movie) => {
+        const { title, poster_path, vote_average, overview } = movie
+        const movieEl = document.createElement('div')
+        movieEl.classList.add('movie')
+        movieEl.innerHTML = `
+        <img src="${IMG_PATH + poster_path}" class="movie-img" alt="${title}">
+        <div class = 'movie-info'>
+        <h3>${title}</h3>
+        <span class="${getClassByRate(vote_average)}">${vote_average}</span>
+        </div>
+        <div class="overview">
+        <h3>Overview</h3>
+        ${overview}
+        </div>
+        `
+        main.appendChild(movieEl)
+    })
+}
+function getClassByRate(vote) {
+    if (vote >= 8) {
+        return 'green'
+    } else if (vote >= 5) {
+        return 'orange'
+    } else {
+        return 'red'
+    }
+}
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    const searchTerm = search.value;
+    if (searchTerm && searchTerm !== '') {
+        getMovies(SEARCH_API + searchTerm)
+        search.value = ""
+    }
+else {
+    window.location.reload()
+}
+})
+
